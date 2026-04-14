015fd55ec22f69f54846f0ed1558351272c043f2
Youssef Micheal <YoussefMicheal@aucegypt.edu>
2026-04-14 17:34:25 +0200

    Fixed the validation issue in the signup

--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 
 int main() {
-//    bool is_valid = false;
-    while (!is_valid) {
-        string email;
-        cout << "Please enter your email: ";
-        cin >> email;
-        try {
-            check_email(email);
-            cout << "Email is valid!" << endl;
-            is_valid = true;
-        } catch (invalid_argument& e) {
-            cerr << "Error: " << e.what() << endl;
-        }
-    }
+// //    bool is_valid = false;
+//     while (!is_valid) {
+//         string email;
+//         cout << "Please enter your email: ";
+//         cin >> email;
+//         try {
+//             check_email(email);
+//             cout << "Email is valid!" << endl;
+//             is_valid = true;
+//         } catch (invalid_argument& e) {
+//             cerr << "Error: " << e.what() << endl;
+//         }
+//     }
     map<string, string> user_data;
     bool is_logged_in = false;
     while (true) {
@@ -31,10 +31,8 @@ int main() {
                 if (!is_logged_in) {
                     string email;
                     string password;
-                    cout << "Please enter your email:" << endl;
-                    cin >> email;
+                    bool is_valid = false;
                     while (!is_valid) {
-                        string email;
                         cout << "Please enter your email: ";
                         cin >> email;
                         try {
@@ -60,17 +58,6 @@ int main() {
                     string password2;
                     cout << "Please enter your email:" << endl;
                     cin >> email2;
-                    while (!is_valid) {
-                        cout << "Please enter your email: ";
-                        cin >> email2;
-                        try {
-                            check_email(email2);
-                            cout << "Email is valid!" << endl;
-                            is_valid = true;
-                        } catch (invalid_argument& e) {
-                            cerr << "Error: " << e.what() << endl;
-                        }
-                    }
                     cout << "Please enter your password:" << endl;
                     cin >> password2;
                     if (user_data.count(email2) && user_data[email2] == password2) {
